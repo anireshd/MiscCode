a0717325d8558b3da22eb25c9c831be27985b7c3
FANDANGOUSA\adevchoudhury <adevchoudhury@fandango.com>
2017-08-08 11:13:30 -0700

    code changes

--- PYTHON MongoDBTest/Offers.py
+++ PYTHON MongoDBTest/Offers.py
@@ -7,7 +7,11 @@ db = client.OffersDB
 
 f = open('H:\Fandango\Offers.csv', 'w')
 
-for rest2 in db.bundles.aggregate([{"$group":{"_id":"Offer_id","count":{"$sum":1}}},{"$sort":{"count":-1}}]):
+#pipe = [{'$group': {'_id': None, 'total': {'$sum': '$bundles'}}}]
+
+#db.bundles.aggregate(pipeline=pipe)
+
+for rest2 in db.bundles.aggregate([{"$group":{"_id":"$Offer_id","count":{"$sum":1}}},{"$sort":{"count":-1}}]):
     pprint.pprint('%s - %d' % (rest2["_id"], rest2["count"]))
     f.write('"%s",%d' % (rest2["_id"], rest2["count"]))
     f.write("\n")
--- PYTHON MongoDBTest/Offers2.py
+++ PYTHON MongoDBTest/Offers2.py
@@ -1,15 +1,27 @@
 import pymongo
 
-
 client = pymongo.MongoClient('localhost', 27017)
 
 db = client.OffersDB
 
 f = open('H:\Fandango\Offers2.csv', 'w')
 
-for rest2 in db.bundles.aggregate([{"$group":{"_id":"Offer_id","count":{"$sum":1}}},{"$sort":{"count":-1}}]):
-    f.write('"%s",%d' % (rest2["_id"], rest2["count"]))
-    f.write("\n")
+varBundle = []
 
-f.write("total = %d" % db.bundles.count())
+for bundles1 in db.bundles.aggregate([{"$group": {"_id": "$Offer_id", "count": {"$sum": 1}}}, {"$sort": {"count": -1}}]):
+    varBundle.append(bundles1)
+
+for offers1 in db.offers.find({}, {"Offer_id": 1, "Offer_nm": 1, "_id": 0}):
+
+   for x in varBundle:
+
+       if offers1["Offer_id"] == x["_id"]:
+            print('"%s","%s","%s", %d' % (offers1["Offer_nm"], offers1["Offer_id"], x["_id"], x["count"]))
+            f.write('"%s","%s",%d' % (offers1["Offer_nm"], x["_id"], x["count"]))
+            f.write("\n")
+
+f.write("Total Bundles= %d" % db.bundles.count())
+f.write("\n")
+f.write("Total Offers= %d" % db.offers.count())
 f.close()
+
